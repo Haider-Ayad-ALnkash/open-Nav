04202392e9f39f99b216fb03d079a2f66da4113d
Haider ALnkash <haiderayadit@gmail.com>
2021-07-31 17:09:40 +0300

    up load code

--- code.js
+++ code.js
@@ -1,12 +1,21 @@
-var heading=document.getElementsByClassName('accordion');
-for (let i = 0; i < heading.length; i++) {
-    heading[i].addEventListener('click',function () {
-        this.classList.toggle('active');
-       var desc=this.nextElementSibling;
-        if (desc.style.maxHeight) {
-           desc.style.maxHeight = null
-       } else {
-        desc.style.maxHeight=desc.scrollHeight +"px";
-       }
-    })  
+// Get All vars 
+var orginalImage=document.querySelector('img'),
+    modal=document.querySelector(".modal"),
+    btoClose=document.querySelector(".close"),
+    textDesc=document.querySelector(".desc");
+// When click on original image popUp show Image and add description      
+orginalImage.onclick=function () {
+    modal.style.display = "block";
+    showImage.src=this.src;
+    textDesc.innerHTML=this.alt;
+}
+// btn close show image and hide overlay
+btoClose.addEventListener("click",function () {
+    modal.style.display = "none";
+})
+//enter btn ESC close image popup
+document.onkeydown=(e)=>{
+    if (e.keyCode =="27") {
+        modal.style.display = "none"; 
+    }
 }
